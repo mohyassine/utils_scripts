621cb06d4f72507d2ab9e330b9b09e4d72674f5b
mohyassine <yassine.moh@gmail.com>
2025-04-15 23:58:40 -0400

    A script to clone all repos in a BitBucket cloud account.

---
+++ bitbucket_cloner.sh
@@ -0,0 +1,28 @@
+#!/bin/zsh
+
+USER=${1}
+TEAM=${2}
+
+rm -rf "$TEAM" && mkdir "$TEAM" && cd $TEAM
+
+NEXT_URL="https://api.bitbucket.org/2.0/repositories/${TEAM}?pagelen=100"
+
+while [ ! -z $NEXT_URL ] && [ $NEXT_URL != "null" ]
+do
+    curl -u $USER $NEXT_URL > repoinfo.json
+    jq -r '.values[] | .links.clone[0].href' repoinfo.json > ../repos.txt
+    NEXT_URL=`jq -r '.next' repoinfo.json`
+
+    for repo in `cat ../repos.txt`
+    do
+        echo "Cloning" $repo
+        if echo "$repo" | grep -q ".git"; then
+            command="git"
+        else
+            command="hg"
+        fi
+        $command clone $repo
+    done
+done
+
+cd ..
